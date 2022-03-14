9d6d3f3ec77d3895a9640f67ee278db06b8ea106
TheFirstHokage <kendrickutwhite@gmail.com>
2022-03-14 02:07:06 -0700

    initial commit

---
+++ spring-pet-clinic/src/test/java/master/spring/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package master.spring.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
